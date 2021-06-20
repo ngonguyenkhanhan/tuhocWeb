c01ddb94b2dfe07606ab0da01e3b24d1593ad0b3
ngonguyenkhanhan <ngonguyenkhanhan@gmail.com>
2021-06-20 22:15:19 +0700

    trên 1 cột, click lần 1 thì ô cuối cùng được tô, click lần 2 thì ô phía trên được tô, click lần 3 thì ô phía trên trên được tô

--- JQuery/gameJQueryjs.js
+++ JQuery/gameJQueryjs.js
@@ -1,6 +1,38 @@
 // var nguoiChoi1 =  prompt("Người chơi 1, Hãy nhập tên của bạn, bạn sẽ là màu XANH")
 // var nguoiChoi2 =  prompt("Người chơi 2, Hãy nhập tên của bạn, bạn sẽ là màu ĐỎ")
 
-$('.board button').on('click', function(){
-    console.log($(this).closest('td').index())
+
+
+//lấy vị trí của btn sau mỗi lần nhấn
+// Vị trí dòng là các tr trong table, vị trí cột là các td trong tr
+// var posBtnClick = [0,0]
+// $('button').on('click', function(){ 
+//     var row = $(this).closest('tr').index() //vị trí dòng
+//     var col = $(this).closest('td').index() //vị trí cột
+//     posBtnClick[0] = row
+//     posBtnClick[1] = col
+// })
+var colorPlayer1 = 'rgb(255,0,0)'
+var colorPlayer2 = 'rgb(0,0,255)'
+$('button').click(function(){
+    var dong = $(this).closest('tr').index() //vị trí dòng
+    var cot = $(this).closest('td').index() //vi tri cot
+    
+    for(row = 5; row > -1; row--){
+        var colorCurrent = returnColor(row, cot)
+        if (colorCurrent === 'rgb(128, 128, 128)') {    //neu mau hien tai la mau xam
+            changeColor(row, cot, colorPlayer1) //thay doi mau
+            beark
+        }
+    }
 })
+
+var tableRow = $('table tr') 
+function returnColor(dong, cot){
+    return tableRow.eq(dong).find('td').eq(cot).find('button').css('background-color')
+}
+function changeColor(dong, cot, color){
+    return tableRow.eq(dong).find('td').eq(cot).find('button').css('background-color',color)
+}
+// console.log(returnColor(5,0))
+
